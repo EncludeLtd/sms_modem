9caf4f6190c23791947c1f02fe4043e5524db483
brandon.dematteis@enclude.ie <brandon.dematteis@enclude.ie>
2020-12-02 13:44:03 +0000

    added setup script

---
+++ os_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Starting Setup..."
+# Install Docker
+sudo apt update &&
+sudo apt install apt-transport-https ca-certificates curl software-properties-common &&
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &&
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" &&
+sudo apt update &&
+apt-cache policy docker-ce &&
+sudo apt install docker-ce &&
+# Install Docker-Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
+sudo chmod +x /usr/local/bin/docker-compose &&
+# Install TeamViewer
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb &&
+sudo apt install ./teamviewer_amd64.deb
